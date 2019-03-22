bbba25a8f2fd2fe1544cd832a789f5efd6af2163
spgdaman <gatheru.simon432@gmail.com>
2019-03-22 21:10:09 +0300

    Update quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,10 +9,10 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes = [
-    new Quote(1, "You are the bomb", "myself", "Simon Gatheru",0,0),
-    new Quote(2, "You are the bombs", "himuselfu", "Simon Gatheru",0,0),
-    new Quote(3, "You are the bombed", "thyself", "Simon Gatheru",0,0),
-    new Quote(4, "You are the bomber", "themself", "Simon Gatheru",0,0)
+    new Quote(1, "The Way Get Started Is To Quit Talking And Begin Doing.", "Walt Disney", "Simon Gatheru",0,0),
+    new Quote(2, "The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.", "Winston Churchill", "Simon Gatheru",0,0),
+    new Quote(3, "People Who Are Crazy Enough To Think They Can Change The World, Are The Ones Who Do.", "Rob Siltanen", "Simon Gatheru",0,0),
+    new Quote(4, "Whether You Think You Can Or Think You Can’t, You’re Right.", "Henry Ford", "Simon Gatheru",0,0)
   ];
 
   toaQuote(isDone, index) {
