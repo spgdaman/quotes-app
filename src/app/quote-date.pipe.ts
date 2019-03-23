8bc7a7c2da7131ab773dba60157545581bb23cca
spgdaman <gatheru.simon432@gmail.com>
2019-03-23 11:28:49 +0300

    Add quoteDate logic in quote-date custom pipe

--- src/app/quote-date.pipe.ts
+++ src/app/quote-date.pipe.ts
@@ -5,8 +5,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class QuoteDatePipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
-  }
+  transform(value: any): number {
+    let today: Date = new Date(); //today's date
+    let todayWithNoTime: any = new Date(today.getFullYear(), today.getMonth(), today.getDate()); //today's date with no time
+    var dateDifference = Math.abs(value - todayWithNoTime) //returns value in milliseconds
+    const dailySeconds = 86400; //Number of seconds in a day
+
+    var dateDifferenceSeconds = dateDifference * 0.001; //converts to seconds
+
+    var dateCounter = dateDifferenceSeconds/dailySeconds;
+
+    if(dateCounter >= 1 && value > todayWithNoTime) {
+      return dateCounter;
+    } else {
+      return 0;
+    }
 
 }
