e4495f8c4b4ebce94f2a976375ae1011de4347a8
spgdaman <gatheru.simon432@gmail.com>
2019-03-23 10:50:36 +0300

    Add date property to quotes class and property binding for the date in quote description component

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,8 @@
 export class Quote {
   public showQuoteDetails: boolean;
   constructor(public id: number, public quote: string, public author: string,
-    public submittor: string, public upvote: number, public downvote: number) {
+    public submittor: string, public upvote: number, public downvote: number,
+    public quoteDate: Date) {
     this.showQuoteDetails = false;
   }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,10 +9,10 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes = [
-    new Quote(1, "The Way Get Started Is To Quit Talking And Begin Doing.", "Walt Disney", "Simon Gatheru",0,0),
-    new Quote(2, "The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.", "Winston Churchill", "Simon Gatheru",0,0),
-    new Quote(3, "People Who Are Crazy Enough To Think They Can Change The World, Are The Ones Who Do.", "Rob Siltanen", "Simon Gatheru",0,0),
-    new Quote(4, "Whether You Think You Can Or Think You Can’t, You’re Right.", "Henry Ford", "Simon Gatheru",0,0)
+    new Quote(1, "The Way Get Started Is To Quit Talking And Begin Doing.", "Walt Disney", "Simon Gatheru",0,0, new Date(2019,0,15)),
+    new Quote(2, "The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.", "Winston Churchill", "Simon Gatheru",0,0, new Date(2019,1,7)),
+    new Quote(3, "People Who Are Crazy Enough To Think They Can Change The World, Are The Ones Who Do.", "Rob Siltanen", "Simon Gatheru",0,0, new Date(2019,2,22)),
+    new Quote(4, "Whether You Think You Can Or Think You Can’t, You’re Right.", "Henry Ford", "Simon Gatheru",0,0, new Date(2019,1,18))
   ];
 
   toaQuote(isDone, index) {
